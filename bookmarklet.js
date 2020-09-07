cc9f07c4eeac1794b7e4c4e66e0f7173ef5be4a8
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-09-07 19:30:23 +0000

    Minor refactor

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(()=>{var s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/Ray-Adams/TTM-Prototype-Bot@latest/bot.js';document.body.appendChild(s)})()
+javascript:{let s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/Ray-Adams/TTM-Prototype-Bot@latest/bot.js';document.body.appendChild(s)}
