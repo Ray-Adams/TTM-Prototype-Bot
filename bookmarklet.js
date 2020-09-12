e9572ca28d805ee059eb209741be9528624594ed
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-09-12 16:42:34 +0000

    Add void

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:{let s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/Ray-Adams/TTM-Prototype-Bot@latest/bot.js';document.body.appendChild(s)}
+javascript:void((()=>{let s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/Ray-Adams/TTM-Prototype-Bot@latest/bot.js';document.body.appendChild(s)})())
